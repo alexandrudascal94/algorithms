4757b5054aa57a55faeae4426b1afbb6ed8e2490
Alexandru Dascal <alexandru.dascal@payconiq.com>
2022-01-11 16:23:32 +0100

    add KClosestPointsOrigin solution

---
+++ src/main/java/arrays/KClosestPointsOrigin.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.PriorityQueue;
+
+public class KClosestPointsOrigin {
+    // https://leetcode.com/problems/k-closest-points-to-origin/
+
+    // we will use a maxHeap to store the first K elements
+    // th maxHeap will use the formula for comparing;
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap =
+                new PriorityQueue<int[]>((a, b) ->  distanceFromOrigin(b) - distanceFromOrigin(a));
+
+        for(int i = 0; i < points.length; i++){
+            maxHeap.add(points[i]);
+
+            if(maxHeap.size() > k){
+                maxHeap.poll();
+            }
+        }
+        // Return all points stored in the max PQ
+        int[][] answer = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            answer[i] = maxHeap.poll();
+        }
+        return answer;
+    }
+
+    public Integer distanceFromOrigin(int[] point){
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
--- src/main/java/arrays/KClosestPointsOrigin_ToSolve.java
+++
@@ -1,5 +0,0 @@
-package arrays;
-
-public class KClosestPointsOrigin_ToSolve {
-    // https://leetcode.com/problems/k-closest-points-to-origin/
-}
